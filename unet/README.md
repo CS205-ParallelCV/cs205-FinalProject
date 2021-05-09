6864d39b34bc37391c2b22f758bb0ff5c6c6a749
Xincheng Tan <caratan1998@yahoo.com>
2021-05-08 21:29:33 -0400

    working code for tf2 with profiling in readme

--- unet/README.md
+++ unet/README.md
@@ -1,15 +1,20 @@
-# cs205-FinalProject
-This is a repo for CS 205 final project.
+# CS205 Final Project (UNet for Nuclei Segmentation)
+This is a repo for UNet implementation in CS205 final project. 
+The primary infrastructure is Google Cloud Platform.
   
 
 
-download data: (need ```gcloud init``` with the account that can access the bucket)
+## Download data: 
+Assume the dataset is saved in Google Cloud Storage bucket 'cs205_final_project'.
+To run the code, first download the data to local machine by the following commands:
 ```
-gsutil cp -R gs://cs205_final_project/cell_imgs data/
-gsutil cp -R gs://cs205_final_project/mask_imgs data/
-gsutil cp -R gs://cs205_final_project/test_imgs data/
+gsutil -m cp -R gs://cs205_final_project/cell_imgs data/
+gsutil -m cp -R gs://cs205_final_project/mask_imgs data/
+gsutil -m cp -R gs://cs205_final_project/test_imgs data/
 ```
-setup environment:
+Note: need ```gcloud init``` with the account that can access the bucket
+
+## Setup environment:
 ```
 python3 --version
 ```
@@ -23,13 +28,68 @@ python3 -m pip install --upgrade pip setuptools
 pip install -r requirements.txt
 pip install google-cloud-profiler --ignore-installed
 ```
-
-To run the code, use the command ```python3 main.py --root_dir <path> --epochs <num> --weights <str>```
-- root_dir: parent directory that contains cell_imgs, mask_imgs, test_imgs  
+   
+## Code Execution
+To run the code, use the command 
+```
+python3 main.py --data_dir <path> --output_dir <path> --log_dir <path> --model_name <str> --epochs <num>
+```
+Flag descriptions:
+- data_dir: directory that contains the datasets: cell_imgs, mask_imgs, test_imgs  
+- log_dir: directory to save tensorboard logs
+- output_dir: directory to save example prediction outputs
 - epochs: number of training epochs  
-- weights: name of the model to be saved, must end with '.h5'!  
+- model_name: name of the model to be saved
 
 For example, 
 ```
-python3 main.py --root_dir /home/user/data --epochs 20 --weights model.h5
+python3 main.py --root_dir /home/user/data --log_dir /home/user/logs --output_dir /home/user/outs --epochs 5 --model_name unet_ep5
 ```
+## Code Profiling
+We use cProfile and TensorBoard for code profiling. 
+
+### cProfile
+Use the following commands to run the main file with cProfile.
+```
+python3 -m cProfile -o main.profile main.py --root_dir xxx --output_dir xxx --epochs 5
+```
+The code profile would be saved to the file 'main.profile'. To view its contents, run the following command:
+```
+python3 -m pstats main.profile
+>> main.profile%
+```
+You will then enter an interactive mode. To view the top 10 most time-consuming functions, 
+within the interactive mode, type the following commands after the percentage symbol in order:
+```
+main.profile%  strip
+main.profile%  sort
+main.profile%  sort time
+main.profile%  stats 10
+```
+
+### TensorBoard
+We also use TensorBoard to visualize the model-specific profiling. 
+To view the logs, first download the logs folder you specified after ```--log_dir``` to your local machine.
+To download, assume you have set up Google Cloud SDK locally. Use the following command to download the logs 
+to your current directory:
+```
+gcloud config set project <your-project-id>
+gcloud compute scp --recurse your-instance:/dir/to/logs .
+```
+Then, make sure you have the correct version of Tensorboard installed by running the following:
+```
+pip uninstall tensorboard
+pip install tensorboard==2.2.0
+pip install -U tensorboard-plugin-profile==2.2.0
+```
+Then use the following command to launch tensorboard:
+```
+tensorboard --logdir dir/to/log
+>> TensorBoard 2.2.0 at http://localhost:6006/ (Press CTRL+C to quit)
+```
+TensorBoard should now be running at ```http://localhost:6006/```. 
+Paste the url in Chrome to visualize the execution graphs.
+  
+For profile information, select the PROFILE tab in the orange header bar and you can see a detailed
+breakdown of the time spent in different sections. More detailed information can be found in
+the 'Tools' drop-down list at the left.
--- unet/main.py
+++ unet/main.py
@@ -1,8 +1,10 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from tqdm import tqdm
+from skimage.io import imshow
 from skimage.transform import resize
 
 from keras.models import Model, load_model
@@ -16,6 +18,8 @@ from evals import prob_to_rles
 
 import tensorflow as tf
 #import googlecloudprofiler
+import pstats
+import cProfile
 
 
 # Set some parameters
@@ -28,37 +32,75 @@ IMG_CHANNELS = 3
 seed = 42
 
 
-if __name__ == "__main__":
-  # Profiler initialization. It starts a daemon thread which continuously
-  # collects and uploads profiles. Best done as early as possible.
-  # try:
-  #   googlecloudprofiler.start(
-  #           service='hello-profiler',
-  #           service_version='1.0.1',
-  #           # verbose is the logging level. 0-error, 1-warning, 2-info,
-  #           # 3-debug. It defaults to 0 (error) if not set.
-  #           verbose=3,
-  #           # project_id must be set if not running on GCP.
-  #           # project_id='my-project-id',
-  #       )
-  # except (ValueError, NotImplementedError) as exc:
-  #   print(exc)  # Handle errors here
-
-
+def parse_to_argdict():
   parser = argparse.ArgumentParser(description='Training Script')
   parser.add_argument('--data_dir', type=str, required=True)
-  parser.add_argument('--log_dir', default='./', type=str, required=True)
+  parser.add_argument('--output_dir', type=str, required=True)
+  parser.add_argument('--model_name', default='unet_model', type=str, required=False)
   parser.add_argument('--epochs', default=20, type=int, required=False)
-  parser.add_argument('--weights', default='unet_model.h5', type=str, required=False)
   args = vars(parser.parse_args())
 
-  ROOT_DIR = args['data_dir']
-  TRAIN_PATH = ROOT_DIR + '/cell_imgs/'
-  MASK_PATH = ROOT_DIR + '/mask_imgs/'
-  TEST_PATH = ROOT_DIR + '/test_imgs/'
+  return args
+
+
+def plot_learning(model_results, savepath):
+
+  plt.figure(figsize=[10, 6])
+  for key in model_results.history.keys():
+    plt.plot(model_results.history[key], label=key)
+
+  plt.legend()
+  plt.savefig(savepath)
+
+
+def show_images(i, ti, orgimg, y_true, preds, preds_t, savename):
+
+  if y_true is not None:
+    f = plt.figure(figsize=(10,10))
+    plt.subplot(221)
+    imshow(orgimg[i])
+    plt.title('Image to be Segmented')
+    plt.subplot(222)
+    imshow(y_true[ti])
+    plt.title('Segmentation Ground Truth')
+    plt.subplot(223)
+    imshow(preds[ti])
+    plt.title('Predicted Segmentation')
+    plt.subplot(224)
+    imshow(preds_t[ti])
+    plt.title('Thresholded Segmentation')
+    f.savefig(savename)
+  else:
+    f = plt.figure(figsize=(12, 7))
+    plt.subplot(131)
+    imshow(orgimg[i])
+    plt.title('Image to be Segmented')
+    plt.subplot(132)
+    imshow(preds[ti])
+    plt.title('Predicted Segmentation')
+    plt.subplot(133)
+    imshow(preds_t[ti])
+    plt.title('Thresholded Segmentation')
+    f.savefig(savename)
+
+
+
+def main():
+  # Read input args
+  args = parse_to_argdict()
+
+  DATA_DIR = args['data_dir']
+  OUTPUT_DIR = args['output_dir']
+  MODEL_NAME = args['model_name']
   EPOCHS = args['epochs']
-  WEIGHTS = args['weights']
-  LOG_DIR = args['log_dir']
+
+  TRAIN_PATH = DATA_DIR + '/cell_imgs/'
+  MASK_PATH = DATA_DIR + '/mask_imgs/'
+  TEST_PATH = DATA_DIR + '/test_imgs/'
+
+  WEIGHTS = OUTPUT_DIR + '/' + MODEL_NAME +'_weights.h5'
+  LOG_DIR = OUTPUT_DIR + "/logs"
+  RESULT_DIR = OUTPUT_DIR + '/results'
 
   # Load train test data
   X_train, Y_train, X_test, train_ids, test_ids, sizes_test = load_train_test(TRAIN_PATH, MASK_PATH, TEST_PATH,
@@ -76,24 +118,76 @@ if __name__ == "__main__":
   checkpointer = ModelCheckpoint(WEIGHTS, verbose=1, save_best_only=True)
   tensorboard = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1, profile_batch='500,520')
 
-  results = model.fit_generator(train_generator, validation_data=val_generator, validation_steps=10,
-                                steps_per_epoch=200,
-                                epochs=EPOCHS, callbacks=[tensorboard, earlystopper, checkpointer])
+  model_results = model.fit_generator(train_generator, validation_data=val_generator, validation_steps=10,
+                                      steps_per_epoch=200, epochs=EPOCHS,
+                                      callbacks=[tensorboard, earlystopper, checkpointer])
+
+  # Plot learning curve
+  plot_learning(model_results, savepath = RESULT_DIR + "/learning_curve.png")
 
   # Predict on train, val and test
-  model = load_model(WEIGHTS) #custom_objects={'mean_iou': mean_iou}
+  model = load_model(WEIGHTS)  # custom_objects={'mean_iou': mean_iou}
   preds_train = model.predict(X_train[:train_size], verbose=1)
   preds_val = model.predict(X_train[train_size:], verbose=1)
   preds_test = model.predict(X_test, verbose=1)
+
+  # Validation Loss and Acc
+  val_results = model.evaluate(X_train[train_size:], Y_train[train_size:], batch_size=BATCH_SIZE)
+  print("Validation Loss:", val_results[0])
+  print("Validation Accuracy :", val_results[1] * 100, "%")
 
   # Threshold predictions
   preds_train_t = (preds_train > 0.5).astype(np.uint8)
   preds_val_t = (preds_val > 0.5).astype(np.uint8)
   preds_test_t = (preds_test > 0.5).astype(np.uint8)
 
-  val_results = model.evaluate(X_train[train_size:], Y_train[train_size:], batch_size=BATCH_SIZE)
-  print("Test Loss:", val_results[0])
-  print("Test Acc :", val_results[1] * 100, "%")
+
+  # Save some example prediction results
+  i = 602
+  show_images(i, i, X_train, Y_train, preds_train, preds_train_t, savename=RESULT_DIR + '/train%d_pred.png' % i)
+
+  i = 662
+  show_images(i, i, X_train, Y_train, preds_val, preds_val_t, savename=RESULT_DIR + '/val%d_pred.png' % i)
+
+  i = 18
+  show_images(i, i, X_test, None, preds_test, preds_test_t, savename=RESULT_DIR + '/test%d_pred.png' % i)
+
+  return
+
+
+if __name__ == "__main__":
+
+  main()
+
+  # In cmdline:
+  # 1. python -m cProfile -o main.profile main.py
+  # 2. python -m pstats main.profile
+  # 3. strip
+  # 4. sort
+  # 5. sort time
+  # 6. stats 10
+
+
+
+  # cProfile.run('main()', 'main.profile')
+  # p = pstats.Stats('restats')
+  # p.strip_dirs().sort_stats(SortKey.TIME).print_stats(10)  # SortKey.CUMULATIVE
+
+  # Profiler initialization. It starts a daemon thread which continuously
+  # collects and uploads profiles. Best done as early as possible.
+  # try:
+  #   googlecloudprofiler.start(
+  #           service='hello-profiler',
+  #           service_version='1.0.1',
+  #           # verbose is the logging level. 0-error, 1-warning, 2-info,
+  #           # 3-debug. It defaults to 0 (error) if not set.
+  #           verbose=3,
+  #           # project_id must be set if not running on GCP.
+  #           # project_id='my-project-id',
+  #       )
+  # except (ValueError, NotImplementedError) as exc:
+  #   print(exc)  # Handle errors here
+
 
   # # Compute Accuracy of on train and validation set
   # iou_train, iou_val = 0, 0
@@ -108,17 +202,17 @@ if __name__ == "__main__":
   # print("Average mean IoU on training set: ", iou_train)
   # print("Average mean IoU on validation set: ", iou_val)
 
-  # Create list of upsampled test masks
-  preds_test_upsampled = []
-  for i in range(len(preds_test)):
-    preds_test_upsampled.append(resize(np.squeeze(preds_test[i]),
-                                       (sizes_test[i][0], sizes_test[i][1]),
-                                       mode='constant', preserve_range=True))
-
-  new_test_ids = []
-  rles = []
-  for n, id_ in enumerate(test_ids):
-    rle = list(prob_to_rles(preds_test_upsampled[n]))
-    rles.extend(rle)
-    new_test_ids.extend([id_] * len(rle))
+  # # Create list of upsampled test masks
+  # preds_test_upsampled = []
+  # for i in range(len(preds_test)):
+  #   preds_test_upsampled.append(resize(np.squeeze(preds_test[i]),
+  #                                      (sizes_test[i][0], sizes_test[i][1]),
+  #                                      mode='constant', preserve_range=True))
+  #
+  # new_test_ids = []
+  # rles = []
+  # for n, id_ in enumerate(test_ids):
+  #   rle = list(prob_to_rles(preds_test_upsampled[n]))
+  #   rles.extend(rle)
+  #   new_test_ids.extend([id_] * len(rle))
 
